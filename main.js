8e529bc571f6033fa6a95a7516f78e8fbe695757
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-16 15:18:09 -0500

    end of module 02

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log("ready!");
-})
+  $("#friend-form").submit( (e) => {
+    e.preventDefault();
+    let newFriend = $('#input').val();
+    let newElement = createFriend(newFriend);
+    $('#friend-list').append(newElement);
+    $('#input').val("");
+    console.log(e.currentTarget[0].value);
+  })
+
+  function createFriend(name) {
+    return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+  }
+});
+
+
