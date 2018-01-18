e1e0dcf31bc85abe5321aae7fe0dd24a6c7e49de
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-17 21:04:19 -0500

    end of module 05

--- main.js
+++ main.js
@@ -5,20 +5,49 @@
 
 
 $(document).ready(function() {
+  $.ajax({
+    url:'http://rest.learncode.academy/api/1150/friends',
+    type: 'GET'
+  }).done( (data) => {
+    data.forEach( (eachFriend) => {
+      let name = eachFriend.firstName + " " + eachFriend.lastName;
+      let newElement = createFriend(name);
+      $('#friend-list').append(newElement);
+    })
+  }).fail( () => {
+    alert('AJAX call failed, unable to retrieve friends.');
+  })
+
   $("#friend-form").submit( (e) => {
     e.preventDefault();
     let newFriend = $('#input').val();
-    let newElement = createFriend(newFriend);
-    $('#friend-list').append(newElement);
-    $('#input').val("");
-    console.log(e.currentTarget[0].value);
+    
+    // Split the name based on the first space we find
+    let firstName = newFriend.substr(0, newFriend.indexOf(' '));
+    let lastName = newFriend.substr(newFriend.indexOf(' ')+1);
+    // Construct the ajax request
+    $.ajax({
+      url: 'http://rest.learncode.academy/api/1150/friends',
+      type: 'POST',
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        email: `${firstName}@${lastName}.com`
+    }
+    }).done( (data) => {
+      console.log(data);
+    }).fail( () => {
+      alert('AJAX call failed, unable to POST new friend');
+    })
   })
-  $('#friend-list').on('click', 'li', function(e) {
-    $(this).remove();
-  })
+  // $('#friend-list').on('click', 'li', function(e) {
+  //   $(this).remove();
+  // })
   function createFriend(name) {
     return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
   }
+
+
+
+
 });
-
-
