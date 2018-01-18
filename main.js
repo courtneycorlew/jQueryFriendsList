a719842984026360326abc6ec43b14f731c8ef7c
CourtneyCorlew <courtneyecorlew@gmail.com>
2018-01-17 21:21:04 -0500

    End of module 06

--- main.js
+++ main.js
@@ -3,29 +3,35 @@
 // //Stuff Hapens
 // })
 
-
+// Waits until the DOM is loaded, and then runs out JavaScript
 $(document).ready(function() {
+
+  // On page load, make an AJAX request to our API endpoint to retrieve all friends
   $.ajax({
     url:'http://rest.learncode.academy/api/1150/friends',
     type: 'GET'
   }).done( (data) => {
+    // When we get an array of friends back, iterate over the array
     data.forEach( (eachFriend) => {
       let name = eachFriend.firstName + " " + eachFriend.lastName;
-      let newElement = createFriend(name);
+      let newElement = createFriend(name, eachFriend.id);
+      // Append that new list-item to our unordered list
       $('#friend-list').append(newElement);
     })
   }).fail( () => {
+    // If our request fails, alert the user
     alert('AJAX call failed, unable to retrieve friends.');
   })
 
+  // On page load, make a click event listener for our Add A New Friend button
   $("#friend-form").submit( (e) => {
+    // Stop the page from refreshing, and then get the value from the input field
     e.preventDefault();
     let newFriend = $('#input').val();
-    
     // Split the name based on the first space we find
     let firstName = newFriend.substr(0, newFriend.indexOf(' '));
     let lastName = newFriend.substr(newFriend.indexOf(' ')+1);
-    // Construct the ajax request
+    // Send the new friend's information to the API
     $.ajax({
       url: 'http://rest.learncode.academy/api/1150/friends',
       type: 'POST',
@@ -35,16 +41,43 @@ $(document).ready(function() {
         email: `${firstName}@${lastName}.com`
     }
     }).done( (data) => {
+      // When the API sends us the success message, construct a list-item and add it to the unordered list
       console.log(data);
+      let newElement = createFriend(newFriend, data.id);
+      $('#friend-list').append(newElement);
+      // Clear the input field
+      $('#input').val("");
     }).fail( () => {
+      // If our request fails, alert the user
       alert('AJAX call failed, unable to POST new friend');
     })
+
   })
-  // $('#friend-list').on('click', 'li', function(e) {
-  //   $(this).remove();
-  // })
-  function createFriend(name) {
-    return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
+
+  // On page load, make a click event listener for our unordered list
+  // When someone clicks a list-item, this function will fire
+  $('#friend-list').on('click', 'li', function(e) {
+    // Make an ajax request to delete the friend from the API
+    // We get the ID from the click event details
+    $.ajax({
+      url: `http://rest.learncode.academy/api/1150/friends/${e.target.id}`,
+      type: 'DELETE'
+    }).done( () => {
+      // When the ajax requests succeeds, remove this list-item from the HTML
+      $(this).remove();
+    }).fail( () => {
+      // If our request fails, alert the user
+      alert('AJAX call failed, unable to DELETE friend')
+    })
+    // console.log(e.target);
+  
+  })
+
+
+  // This function creates an <li> HTML element for a friend, given a name, and an ID
+  // Also applies CSS classes to the element for appropriate styling
+  function createFriend(name,id) {
+    return $(`<li> ${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark').attr('id', id);
   }
 
 
